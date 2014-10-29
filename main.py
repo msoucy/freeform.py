e0a92cfdc11b6e8335386f08fc600b847f099285
Ross Delinger <rossdylan@csh.rit.edu>
2014-10-29 09:25:53 -0400

    added pylint support for selflinting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from urllib import urlopen
 from json import loads as jsonloads
+from pylint import epylint as lint
+
 
 def last_commit_message():
     """ Gets the message from the latest commit from the msoucy/freeform.py
@@ -28,11 +30,20 @@ def last_commit_message():
     # return the last committer
     return jobj[0][u'author'][u'login'] if jobj else None
 
+def lint_the_things():
+    (pylint_stdout, pylint_stderr) = lint.py_run(__file__, True)
+    lines = [line for line in pylint_stdout.readlines() if not line.startswith('***')]
+    print("PyLint Results")
+    for line in lines:
+        print("\t{0}".format(line))
+    return lines
+
 import this
 
 
 def main():
     last_author = last_commit_message()
+    lint_result = lint_the_things()
     pass
 
 if __name__ == '__main__':
